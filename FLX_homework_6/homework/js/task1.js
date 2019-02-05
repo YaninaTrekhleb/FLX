12484477ef01dd2712c0d648e3fdd3b8454f6a15
Yanina Trekhleb <yanisavchenko@gmail.com>
2019-02-05 21:50:23 +0200

    Comleted 1 and 2 tasks.

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,22 +1,19 @@
-function solveQuadraticEquation(a, b, c) {
- 
-}
-
-
 const a = parseInt(prompt('Enter A number'));
 const b = parseInt(prompt('Enter B number'));
 const c = parseInt(prompt('Enter C number'));
 
 const discriminant = b * b - 4 * a * c;
 
+let message = '';
 if (discriminant < 0) {
-    console.log('Error, no solution');
+    message = 'Error, no solution'; 
 } else if (discriminant === 0) {
     const x = -1 * b / (2 * a);
-    console.log(x);
+    message = `x = ${x}, D = ${discriminant}`;
 } else {
-    const x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
-    const x2 = (-1 * b - Math.sqrt(d)) / (2 * a);
-    console.log(x1, x2);
+    const x1 = (-1 * b + Math.sqrt(discriminant)) / (2 * a);
+    const x2 = (-1 * b - Math.sqrt(discriminant)) / (2 * a);
+    message = `x1 = ${x1}, x2 = ${x2}, D = ${discriminant}`;
 }
 
+alert(message);
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,1 +1,25 @@
-// Your code goes here
+const totalPrice = prompt('Enter total price'); 
+const discount = prompt('Enter discount');
+
+let message = '';
+
+if (totalPrice < 0 || totalPrice > 9999999) {
+    message = 'Invalid data';
+} else if (discount > 99) {
+    message = 'Invalid data';
+} else {
+    let priceWithDiscount = totalPrice * ((100 - discount) / 100);
+    priceWithDiscount = Math.round(priceWithDiscount * 100) / 100;
+    
+    let saved = totalPrice - priceWithDiscount;
+    saved = Math.round(saved * 100) / 100;
+    
+    message = `
+    Total price: ${totalPrice},
+    Discount: ${discount}%,
+    Price with discount: ${priceWithDiscount},
+    Saved: ${saved}
+    `;
+}
+
+alert(message);
