00c026d148bc915d2c88600cd772d15707faeac1
Yanina Trekhleb <yanisavchenko@gmail.com>
2019-02-10 16:03:15 +0200

    Made 1-st task.

--- FLX_homework_7/homework/src/js/task1.js
+++ FLX_homework_7/homework/src/js/task1.js
@@ -1,1 +1,23 @@
-// Your code goes here
+let userName = prompt('Enter your User Name');
+if (userName === '' || userName === null) {
+    alert('Canceled');
+} else if (userName.length < 4 ) {
+    alert('I don\'t know any users having name length less than 4 symbols');
+} else if (userName !== 'User' && userName !== 'Admin') {
+    alert('I don\'t know you');
+} else {
+    let userPassword = prompt('Enter your Password');
+    if (userPassword === '' || userPassword === null) {
+        alert('Canceled');
+    } else if (userName === 'User' && userPassword !== 'UserPass') {
+        alert('Wrong password');
+    } else if (userName === 'Admin' && userPassword !== 'RootPass') {
+        alert('Wrong password');
+    } else {
+        const currentHours = new Date().getHours();
+        const partOfDay = currentHours < 20 ? 'day' : 'evening';
+        const greating = `Good ${partOfDay}, dear ${userName}!`;
+        alert(greating);
+    }
+} 
+
